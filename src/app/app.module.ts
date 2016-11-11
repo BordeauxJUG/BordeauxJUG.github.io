7001b1328cb894b3cb4f08e34dc2970e3dc8d689
Benoit Prioux <benoit.prioux@gmail.com>
2016-11-11 16:21:20 +0100

    add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
 
 import { routes } from './app.routing';
 import { AppComponent } from './app.component';
@@ -23,6 +24,7 @@ import { MembersComponent} from './members/members.component';
     BrowserModule,
     FormsModule,
     HttpModule,
+    MaterialModule.forRoot(),
     routes
   ],
   providers: [],
