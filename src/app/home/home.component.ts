c5a8c0c4f12f5e3344f7e86a09907518afe8fd27
Benoit Prioux <benoit.prioux@gmail.com>
2016-11-20 15:59:35 +0100

    add calendar, fix issue #4

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
-import {Meeting} from '../shared/meeting.model';
-import {Sponsor} from '../shared/sponsor.model';
-import {BdxjugService} from '../shared/bdxjug.service';
+import { Meeting } from '../shared/meeting.model';
+import { Sponsor } from '../shared/sponsor.model';
+import { BdxjugService } from '../shared/bdxjug.service';
 
 @Component({
   selector: 'app-home',
@@ -11,13 +11,19 @@ import {BdxjugService} from '../shared/bdxjug.service';
 })
 export class HomeComponent implements OnInit {
 
+  hasUpcommings: boolean;
   upcomings: Meeting[];
   sponsors: Sponsor[];
 
-  constructor(private service: BdxjugService) { }
+  constructor(private service: BdxjugService) {
+    this.hasUpcommings = false;
+  }
 
   ngOnInit() {
-    this.service.getUpcomingMeetings().subscribe(m => this.upcomings = m);
+    this.service.getUpcomingMeetings().subscribe(m => {
+      this.upcomings = m;
+      this.hasUpcommings = m.length > 0;
+    });
     this.service.getSponsors().subscribe(m => this.sponsors = m);
   }
 
