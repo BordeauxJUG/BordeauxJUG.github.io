d772f86d33dcfa196d70d7b03def2bca47aab4dc
Benoit Prioux <benoit.prioux@gmail.com>
2016-11-20 16:24:48 +0100

    add paypal fix issue #2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MembersComponent} from './members/members.component';
 import { PresentationComponent } from './presentation/presentation.component';
 
 import {BdxjugService} from './shared/bdxjug.service';
+import { PaypalComponent } from './paypal/paypal.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import {BdxjugService} from './shared/bdxjug.service';
     HomeComponent,
     MeetingListComponent,
     MeetingDetailComponent,
-    PresentationComponent
+    PresentationComponent,
+    PaypalComponent
   ],
   imports: [
     BrowserModule,
